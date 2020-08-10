ce8f21ea98f693fda26a1cfcdc895a3b83a42037
tree <807596654@qq.com>
2020-08-10 20:40:11 +0800

    feat: add fuzz method FuzzCompile()

--- fuzz.go
+++ fuzz.go
@@ -2,6 +2,13 @@
 
 package ngx
 
+func FuzzCompile(p []byte) int {
+	if _, err := Compile(string(p)); err != nil {
+		return 0
+	}
+	return 1
+}
+
 func FuzzUnmarshal(p []byte) int {
 	if err := Unmarshal(p, new(Access)); err != nil {
 		return 0
